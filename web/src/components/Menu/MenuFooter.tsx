9475dce8b353dcde21fb2247b96ccf6976a0b53e
kitotake <ahmed12199725@gmail.com>
2026-04-29 05:21:44 +0200

    ui

--- web/src/components/Menu/MenuFooter.tsx
+++ web/src/components/Menu/MenuFooter.tsx
@@ -1,6 +1,10 @@
 import styles from './MenuFooter.module.scss'
 
-export function MenuFooter() {
+interface MenuFooterProps {
+  isRoot?: boolean
+}
+
+export function MenuFooter({ isRoot }: MenuFooterProps) {
   return (
     <div className={styles.footer}>
       <div className={styles.hint}>
@@ -8,13 +12,20 @@ export function MenuFooter() {
         <span>Naviguer</span>
       </div>
       <div className={styles.hint}>
-        <kbd className={styles.key}>Entrée</kbd>
-        <span>Sélectionner</span>
+        <kbd className={styles.key}>↵</kbd>
+        <span>Confirmer</span>
       </div>
-      <div className={styles.hint}>
-        <kbd className={styles.key}>Échap</kbd>
-        <span>Fermer</span>
-      </div>
+      {!isRoot ? (
+        <div className={styles.hint}>
+          <kbd className={styles.key}>⌫</kbd>
+          <span>Retour</span>
+        </div>
+      ) : (
+        <div className={styles.hint}>
+          <kbd className={styles.key}>Esc</kbd>
+          <span>Fermer</span>
+        </div>
+      )}
     </div>
   )
 }
